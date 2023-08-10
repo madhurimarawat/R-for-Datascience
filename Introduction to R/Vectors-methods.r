f396619fec3977229a6af8dfdc5b26d43c9257b4
Madhurima Rawat <105432776+madhurimarawat@users.noreply.github.com>
2023-08-10 12:53:27 +0530

    Add files via upload

---
+++ Introduction to R/Vectors-methods.r
@@ -0,0 +1,14 @@
+#1 Vector containing weights
+weight=c(68,73,82,60,88,77)
+print(weight)
+#2 Mean
+print(mean(weight))
+#3 Minimum and Maximum
+print(c(min(weight),max(weight)))
+#4 Weight range
+print(range(weight))
+#5 New vector containing first 3 members
+new_weight=weight[c(1,2,3)]
+print(new_weight)
+#6 Lenth of new vector
+print(length(new_weight))
